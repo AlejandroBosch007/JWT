9530e10b67939050e67526abeba139584786a019
AlejandroBosch007 <tecla@puma.unam.mx>
2021-09-14 19:58:18 -0500

    fin

--- db/conexion.js
+++ db/conexion.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('JsonWT', null, null, {
+const sequelize = new Sequelize('jwt', null, null, {
   dialect: 'mssql',
   server: process.env.DB_HOST,
   port: process.env.DB_PORT,
